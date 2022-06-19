645144d0709ad02d5ec0794b0c00b652dad9a5f7
siye <xuhanfeng@pagoda.com.cn>
2022-06-19 23:42:55 +0800

    give some demo code for generate summer boot code

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,40 @@
 use std::process::Command;
+use std::fs;
+use std::fs::File;
+use std::io::Result;
 
-fn main() {
-   Command::new("cargo")
-   .arg("new")
-   .arg("hello")
-   .output()
-   .expect("command is failed!");
+fn main() -> Result<()> {
+   //Command::new("cargo")
+  // .arg("new")
+   //.arg("hello")
+   //.output()
+  // .expect("command is failed!");
+  fs::create_dir_all("/Users/fenggege/work/opensource/summer-initializr/test/hello/src")?;
+  fs::write("/Users/fenggege/work/opensource/summer-initializr/test/hello/src/main.rs",
+"fn main () {
+   println!(\"hello world\");
+}"
+   )?;
+  fs::write("/Users/fenggege/work/opensource/summer-initializr/test/hello/Cargo.toml"
+  ,"
+  [package]
+  name = \"summer-initializr\"
+  version = \"0.1.0\"
+  edition = \"2021\"
+  
+  # See more keys and their definitions at https://doc.rust-lang.org/cargo/reference/manifest.html
+  
+  [dependencies]
+  cargo = \"0.62\"")?;
+  Ok(())
+}
+
+pub struct Dir {
+   name: String,
+   file: File,
+}
+
+pub struct Package {
+   name: String,
+   file: File,
 }
