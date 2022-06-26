7262f46cbad703e0428fb8b8d35e585bf90d033f
siye <xuhanfeng@pagoda.com.cn>
2022-06-26 19:02:51 +0800

    add summer-boot for http request and addtional some api handle zip file

--- src/main.rs
+++ src/main.rs
@@ -1,82 +1,118 @@
 
 use std::{
    fs,
-   io::{Read, Write},
-   net::TcpListener,
-   thread,
+   fs::File,
+   io::{Read, Write, Seek},
+   process::Command,
+   path::Path,
+   //net::TcpListener,
+   //thread,
 };
+use zip::result::ZipResult;
+use zip::write::{FileOptions, ZipWriter};
 use serde_derive::{Deserialize, Serialize};
-type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
+use walkdir::{DirEntry, WalkDir};
+use summer_boot::{Request, Result};
+use summer_boot::log::{info};
 
-fn main() {
-   let listener = TcpListener::bind("").unwrap();
-   loop {
-      let (mut stream, addr) = listener.accept().unwrap();
-      println!("Accepted a new connection: {}", addr);
-      thread::spawn(move || {
-         let mut buf = [0; std::mem::size_of::<usize>()];
-         stream.read_exact(&mut buf).unwrap();
-         let json_size = usize::from_be_bytes(buf);
-         let mut json_buf = vec![0; json_size];
-         stream.read(&mut json_buf).unwrap();
-         let value: Package = serde_json::from_slice(&json_buf).unwrap();
-         //let data = String::from_utf8_lossy(&buf).to_string();
-         //println!("data:{:?}", serde_json::from_str(&data));
-         stream.write_all(b"").unwrap();
-      });
-   }
-}
 ///包名对象
 #[derive(Deserialize, Serialize)]
 pub struct Package {
    name: String,
-   version: String,
-   edition: String,
-   denpendecies: Vec<Dependency>,
+   version: Option<String>,
+   edition: Option<String>,
+   denpendecies: Option<Vec<Dependency>>,
 }
 ///依赖对象
 #[derive(Deserialize, Serialize)]
 pub struct Dependency {
-   name: String,
-   version: String,
+   name: Option<String>,
+   version: Option<String>,
 }
 
-fn write_buf(package: Package) -> Result<()> {
-   let version = package.version;
-   let name = package.name;
-   let edition = package.edition;
-   let denpendecies = package.denpendecies;
-   let path = "./".to_string() + &name + "/src";
-   fs::create_dir_all(&path)?;
-   let main_contents = "fn main () {
-      println!(\"hello world\");
-   }";
-   fs::write("./".to_string() + &name + "/src/main.rs", main_contents)?;
-   let mut cargo_content = String::new();
-   cargo_content.push_str("[package]\n");
-   cargo_content.push_str("name = ");
-   cargo_content.push_str("\"");
-   cargo_content.push_str(&name);
-   cargo_content.push_str("\"");
-   cargo_content.push_str("\nedition = ");
-   cargo_content.push_str("\"");
-   cargo_content.push_str(&edition);
-   cargo_content.push_str("\"");
-   cargo_content.push_str("\nversion = ");
-   cargo_content.push_str("\"");
-   cargo_content.push_str(&version);
-   cargo_content.push_str("\"");
-   cargo_content.push_str("\n[dependcies]");
-   for d in &denpendecies {
-      cargo_content.push_str(d.name.as_str());
-      cargo_content.push_str(" = ");
-      cargo_content.push_str("\"");
-      cargo_content.push_str(d.version.as_str());
-      cargo_content.push_str("\"");
-   };
-   fs::write("./".to_string()+ &name + "/Cargo.toml", cargo_content)?;
-   
-   Ok(())
+#[summer_boot::auto_scan]
+#[summer_boot::main]
+async fn main() {
+   summer_boot::run();
 }
+
+#[summer_boot::post("/test/api")]
+async fn test_api(mut req: Request<()>) -> Result {
+   let Package {
+      name,
+      version,
+      edition,
+      denpendecies
+   } = req.body_json().await?;
+   command_generation_rust_project(&name);
+   //初始化项目文件路径
+   let path = Path::new(&name);
+   let mut init_zip_path = String::new();
+   init_zip_path.push_str(&name);
+   init_zip_path.push_str(".zip");
+   let zip_path = Path::new(&mut init_zip_path);
+   compress_dir(path, zip_path);
+   //删除文件保留zip文件
+   fs::remove_dir_all(path);
+   let mut file = File::open(zip_path).unwrap();
+   let mut io_byte = Vec::new();
+   file.read_to_end(&mut io_byte);
+   Ok(format!("hello,{:?}", &mut io_byte).into())
+}
+
+fn command_generation_rust_project(filename: &str) {
+    Command::new("cargo")
+    .arg("new")
+    .arg(filename)
+    .output()
+    .expect("cargo new failed");
+}
+
+///压缩文件夹
+fn compress_dir(src_dir: &Path, target: &Path){
+    let zip_file = File::create(target).unwrap();
+    let dir = WalkDir::new(src_dir);
+    //文件压缩
+    zip_dir(&mut dir.into_iter().filter_map(|e|e.ok()),
+     src_dir.to_str().unwrap(), zip_file);
+}
+
+///压缩文件文件夹方法
+fn zip_dir<T>(it: &mut dyn Iterator<Item = DirEntry>, prefix: &str, writer: T) -> 
+    ZipResult<()> 
+        where T: Seek + Write
+{
+    let mut zip = ZipWriter::new(writer);
+    let options = FileOptions::default()
+                              .compression_method(zip::CompressionMethod::Bzip2)//Bzip2格式
+                              .unix_permissions(0o755);//unix权限
+    let mut buf = Vec::new();
+    for entry in it {
+        let path = entry.path();
+        //zip压缩一个文件时，会把它的全路径当成文件名
+        //去掉目录前缀
+        info!("prefix:{:?}...", path);
+        let name = path.strip_prefix(Path::new(prefix)).unwrap();
+        info!("name:{:?}...", name);
+        //如果是文件
+        if path.is_file() {
+            info!("add file {:?} as {:?}..", path, name);
+            zip.start_file_from_path(name, options)?;
+            let mut f = File::open(name)?;
+            //文件读取
+            f.read_to_end(&mut buf)?;
+            zip.write_all(&*buf);
+            buf.clear();
+            //如果是目录
+        } 
+        //else if name.as_os_str().len() != 0 {
+           // println!("add dir {:?} as {:?} ...", path, name);
+           // zip.add_directory_from_path(name, options)?;
+        //}
+    }
+    zip.finish()?;
+     Ok(())
+}
+
 
 
